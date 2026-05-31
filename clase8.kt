421e2be9432724ad4c154ab223e90481431911b4
Daniel Rivera Ríos <juan.daniel.riv@gmail.com>
2026-05-31 00:49:54 -0600

    Ejemplo de onSaveInstanceState con errores de sintaxis

--- clase8.kt
+++ clase8.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsCompat 
 
 class MainActivity : AppCompatActivity() {
 
